8fc5acbe18894ffa7eef79702dbab6d6f13cd1b5
saulsneider7 <saulsneider@hotmail.com>
2023-10-30 20:15:38 -0500

    sesion 1

---
+++ sesion1/sesion1.py
@@ -0,0 +1,39 @@
+# --------------------------------------------------------------------------------------------
+#                                        VARIABLES
+# --------------------------------------------------------------------------------------------
+aedouhua = "Hola mundo\n"
+kjcnkjjda = 5
+qijjibnds = 5.2
+qiwhq = True
+
+numerosPares = [2, 4, 6, 8, 10]
+gggg = [aedouhua, kjcnkjjda, qijjibnds, qiwhq]
+
+print (gggg)
+print(type(aedouhua), type(kjcnkjjda), type(qijjibnds), type(qiwhq))
+print(aedouhua * kjcnkjjda)
+
+# --------------------------------------------------------------------------------------------
+#                                   ESTRUCTURAS DE FLUJO
+# --------------------------------------------------------------------------------------------
+numero = 1
+if numero == 10:
+    print("La condicion es correcta")
+else:
+    print("La condicion no se cumple")
+
+
+
+# ----------------------------------------------------------------
+if numero == 0:
+    print("El numero es cero")
+elif numero == 1:
+    print("El numero es uno")
+elif numero == 2:
+    print("El numero es dos")
+else:
+    print("El numero es negativo")
+
+#ACTVIDAD
+#Crear una variable, y imprimir en la consola el numero de dia de la semana correspondiente 
+# (Ejemplo: 1 = Lunes, 2 = Martes, etc)
