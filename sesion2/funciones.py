faf57a41a6898fb7c6938003c806bd971ebb2f2b
saulsneider7 <saulsneider@hotmail.com>
2023-11-13 19:17:57 -0500

    sesion 3

---
+++ sesion2/funciones.py
@@ -0,0 +1,41 @@
+def saludar():
+    print("Hola!")
+saludar()
+
+
+def saludar2(nombre,apellido):
+    print("Hola", nombre, apellido)
+
+saludar2("Pepito", "Perez")
+
+#Crear una funcion que te imprima el siguiente texto:
+# Mario, tu amigo Juan esta en el parque.
+
+def shdghj(nom1, nom2):
+    print(nom1, "tu amigo ", nom2, "esta en el parque.")
+
+shdghj("Mario", "Juan")
+
+#Crear un funcion Suma que sume dos numeros.
+def suma(a, b):
+    resultado = a + b
+    print(resultado)
+
+# a=int(input("Escribe un numero: "))
+# b=int(input("Escribe otro numero: "))
+
+# suma(a, b)
+
+
+#Crear una funcion que calcule el area de un rectangulo.
+def areaRectangulo(a,b):
+    print(a*b)
+areaRectangulo(10,5)
+
+#Crear una funcion que calcule el area de un cuadrado.
+# El area del cuadrado es _________
+
+def areaCuadrado(x):
+    print("El area del cuadrado es: ", x*x)
+
+areaCuadrado(5)
---
+++ sesion3/POO.py
@@ -0,0 +1,17 @@
+celular1_marca = "Xiaomi"
+celular2_marca = "Samsung"
+celular3_marca = "Apple"
+
+celular1_modelo = "Redmi 9"
+celular2_modelo = "S22"
+celular3_modelo = "Iphone 13"
+
+celular1_color = "Negro"
+celular2_color = "Plata"
+celular3_color = "Rojo"
+
+celular1_camara = "64 MP"
+celular2_camara = "48 MP"
+celular3_camara = "128 MP"
+
+print (celular1_color)
