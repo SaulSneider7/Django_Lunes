9362c7202ed8ece65b561ebbc839751619e55bf0
saulsneider7 <saulsneider@hotmail.com>
2023-11-20 19:11:14 -0500

    sesion 3

--- sesion3/POO.py
+++ sesion3/POO.py
@@ -14,4 +14,38 @@ celular1_camara = "64 MP"
 celular2_camara = "48 MP"
 celular3_camara = "128 MP"
 
-print (celular1_color)
+# print (celular2_marca)
+
+print(
+    '''
+    ===================================================
+    PROGRAMACION ORIENTADA A OBJETOS (POO)
+    ===================================================
+    ''')
+
+class Celular():
+    marca = "Xiaomi"
+    camara = "64 MP"
+    color = "Negro"
+    modelo = "Redmi 9"
+
+celular1 = Celular()
+celular2 = Celular()
+celular3 = Celular()
+print(celular1.color)
+
+class Celulares:
+    def __init__(self, marca, camara, color, modelo):
+        self.marca = marca
+        self.camara = camara
+        self.color = color
+        self.modelo = modelo
+    
+    def llamar(self):
+        return('Estas realizando una llamada con tu ' + self.marca)
+
+celular4 = Celulares("Huawei", "8MP",'Blanco',"P9")
+celular5 = Celulares("Samsung", "18MP",'Negro',"S10")
+
+print(celular5.llamar())
+
